1d810b27d1dc85a5d57dc83f900716b1792a5326
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-08 14:43:37 +0530

    Variable

---
+++ Standard Concept/Variable/VariableType.java
@@ -0,0 +1,11 @@
+class VariableType {
+    public static void main(String args[])
+    {
+        int data = 50;  // Instance Variable
+        static int m = 100; // Static Variable
+        void method()
+        {
+            int n=90; // Local Variable
+        }
+    }
+}
