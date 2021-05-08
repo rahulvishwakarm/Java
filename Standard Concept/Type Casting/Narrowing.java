2be74d633b95768c82a8c406e2d929389e634519
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-08 14:53:08 +0530

    Type Casting

---
+++ Standard Concept/Type Casting/Narrowing.java
@@ -0,0 +1,13 @@
+
+// Narrowing - Narrowing is the process of typecasting higher data type to lower data type
+
+class Narrowing
+{
+    public static void main(String args[])
+    {
+        float f = 10.5f;
+        int a = (int)f;
+        System.out.println(f);
+        System.out.println(a);
+    }
+}
---
+++ Standard Concept/Type Casting/Widning.java
@@ -0,0 +1,12 @@
+
+// Widning - It is the process of typecasting lower data type to higher datatype
+
+class Widning {
+    public static void main(String args[])
+    {
+        int a=10;
+        float f=a;
+        System.out.println(a);
+        System.out.println(f);
+    }
+}
