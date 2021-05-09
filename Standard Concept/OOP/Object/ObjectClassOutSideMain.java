35ac33e0f082665cb4253539d192ab6b9d995eca
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-09 23:50:22 +0530

    Object - 1

---
+++ Standard Concept/OOP/Object/ObjectClassOutSideMain.java
@@ -0,0 +1,15 @@
+class ObjectClassOutSide
+{
+    int id;
+    String name;
+}
+
+class ObjectClassOutSideMain
+{
+    public static void main(String args[])
+    {
+        ObjectClassOutSide OBS = new ObjectClassOutSide();
+        System.out.println(OBS.id);
+        System.out.println(OBS.name);
+    }
+}
---
+++ Standard Concept/OOP/Object/ObjectClassWithInClass.java
@@ -0,0 +1,17 @@
+// Main within Class
+
+//Defining Class
+class ObjectClassWithInClass
+{
+    int id;
+    String str;
+    //creating main method
+    public static void main(String args[])
+    {
+        //creating object or instance
+        ObjectClass OC = new ObjectClass();
+        //accessing membaers of class
+        System.out.println(OC.id);
+        System.out.println(OC.str);
+    }
+}
