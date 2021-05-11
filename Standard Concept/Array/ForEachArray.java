dfcc14cba2addff6bbc3a01b9621ad89bc8c522a
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-11 22:24:02 +0530

    Array

---
+++ Standard Concept/Array/ForEachArray.java
@@ -0,0 +1,11 @@
+class ForEachArray
+{
+    public static void main(String args[])
+    {
+        String arr[] = {"R","A","H","U","L"};
+        for(String j:arr)
+        {
+            System.out.print(j);
+        }
+    }
+}
---
+++ Standard Concept/Array/TraverseArray.java
@@ -0,0 +1,11 @@
+class TraverseArray
+{
+    public static void main(String args[])
+    {
+        int arr[] = {5,78,54,33,45,67};
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
