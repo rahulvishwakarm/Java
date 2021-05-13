9db11cd7168bb5801e751d882d5df5e1d090833e
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-13 21:21:18 +0530

    Recursion - Factorial

---
+++ Data Structures/Recursion/FactorialRec.java
@@ -0,0 +1,20 @@
+class FactorialRec
+{
+    public static int Fact(int n)
+    {
+        if(n==1)
+        {
+            return 1;
+        }   
+        else
+        {
+            return n*Fact(n-1);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int n=5;
+        System.out.println(Fact(n));
+    }
+}
