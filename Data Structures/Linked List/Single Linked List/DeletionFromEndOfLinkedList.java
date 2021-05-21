6ad4042502f1526832178bd8b0612cab3750f9b1
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-21 08:22:01 +0530

    Single Linked list - Deletion From End

---
+++ Data Structures/Linked List/Single Linked List/DeletionFromEndOfLinkedList.java
@@ -0,0 +1,51 @@
+class DeletionFromEndOfLinkedList
+{
+    Node head;
+    static class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        DeletionFromEndOfLinkedList DBL = new DeletionFromEndOfLinkedList();
+        DBL.head = new Node(11);
+        Node second = new Node(22);
+        Node third = new Node(33);
+        Node fourth = new Node(44);
+
+        DBL.head.next = second;
+        second.next = third;
+        third.next = fourth;
+
+        // Traversing
+        Node ptr1 = DBL.head;
+        while(ptr1!=null) {
+            System.out.print(ptr1.data+"\t");
+            ptr1 = ptr1.next;
+        }
+
+        System.out.print("\n");
+
+        // Deleting
+        Node point = DBL.head;
+        while(point.next!=null){
+            ptr1 = point;
+            point = point.next;
+        }
+        ptr1.next = null;
+
+        // Traversing
+        Node ptr2 = DBL.head;
+        while(ptr2!=null) {
+            System.out.print(ptr2.data+"\t");
+            ptr2 = ptr2.next;
+        }
+    }
+}
