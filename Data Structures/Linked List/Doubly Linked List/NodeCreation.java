06f05c61f77548d74bb979ba7777a8355d11aa50
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-05-22 23:03:14 +0530

    Doubly Linked List - Node Creation

---
+++ Data Structures/Linked List/Doubly Linked List/NodeCreation.java
@@ -0,0 +1,20 @@
+class NodeCreation
+{
+    Node head;
+    static class Node
+    {
+        int data;
+        Node prev;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            prev = null;
+            next = null;
+        }
+    }
+    public static void main(String[] args)
+    {
+        NodeCreation NC = new NodeCreation(11);
+    }
+}
