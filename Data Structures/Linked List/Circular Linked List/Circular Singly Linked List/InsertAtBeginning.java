a1a70f5ce2d750c322e1da98e111677289867d87
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-06-01 01:20:57 +0530

    Circular Singly Linked List - Insertion At Beginning Updated

---
+++ Data Structures/Linked List/Circular Linked List/Circular Singly Linked List/InsertAtBeginning.java
@@ -0,0 +1,61 @@
+class InsertAtBeginning
+{
+    static class Node
+    {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    static Node addToNull(Node head,int d)
+    {
+        if (head != null)
+            return head;
+        Node lnk = new Node(d);
+        head = lnk;
+        head.next = head;
+        return head;
+    };
+
+    static Node addToBeginning(Node head,int d)
+    {
+        if(head == null)
+        {
+            return addToNull(head,d);
+        }
+        Node lnk = new Node(d);
+        lnk.next = head.next;
+        head.next = lnk;
+        return head;
+    };
+    
+    static void traverse(Node head)
+    {
+        Node ptr = head.next;
+        if (head == null)
+        {
+            System.out.print("List is empty.");
+            return;
+        }
+        System.out.print("Head->"); 
+        do
+        {
+            System.out.print(ptr.data+"->"); 
+            ptr = ptr.next;
+        }
+        while(ptr!=head.next);
+        System.out.print("Head"); 
+    };
+
+    public static void main(String[] args)
+    {
+        Node head = null;
+        head = addToNull(head,22);
+        head = addToBeginning(head,11);
+        traverse(head);
+    }
+}
